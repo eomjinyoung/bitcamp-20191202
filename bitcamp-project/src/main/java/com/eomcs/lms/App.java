2bb936b401f0f24bfc8ba599b33dd3c295a856ce
eomjinyoung <jinyoung.eom@gmail.com>
2019-12-23 16:07:55 +0900

    2019-12-23

--- bitcamp-project/src/main/java/com/eomcs/lms/App.java
+++ bitcamp-project/src/main/java/com/eomcs/lms/App.java
@@ -7,20 +7,6 @@ public class App {
 
   static Scanner keyboard = new Scanner(System.in);
   
-  static class Member {
-    int no;
-    String name;
-    String email;
-    String password;
-    String photo;
-    String tel;
-    Date registeredDate;
-  }
-  
-  static final int MEMBER_SIZE = 100;
-  static Member[] members = new Member[MEMBER_SIZE];
-  static int memberCount = 0;
-
   static class Board {
     int no;
     String title;
@@ -38,6 +24,10 @@ public class App {
     // 그 메서드가 작업할 때 사용할 키보드 객체를 설정해줘야 한다.
     LessonHandler.keyboard = keyboard;
     
+    // MemberHandler의 메서드를 사용하기 전에 
+    // 그 메서드가 작업할 때 사용할 키보드 객체를 설정해준다.
+    MemberHandler.keyboard = keyboard;
+    
     String command;
     
     do {
@@ -54,10 +44,10 @@ public class App {
           LessonHandler.listLesson();
           break;
         case "/member/add":
-          addMember();
+          MemberHandler.addMember();
           break;
         case "/member/list":
-          listMember();
+          MemberHandler.listMember();
           break;
         case "/board/add":
           addBoard();
@@ -103,41 +93,7 @@ public class App {
     System.out.println("저장하였습니다.");
   }
 
-  static void listMember() {
-    for (int i = 0; i < memberCount; i++) {
-      Member m = members[i];
-      System.out.printf("%d, %s, %s, %s, %s\n", 
-          m.no, m.name, m.email, m.tel, m.registeredDate);
-    }
-  }
 
-  static void addMember() {
-    Member member = new Member();
-
-    System.out.print("번호? ");
-    member.no = keyboard.nextInt();
-    keyboard.nextLine(); // 줄바꿈 기호 제거용
-
-    System.out.print("이름? ");
-    member.name = keyboard.nextLine();
-
-    System.out.print("이메일? ");
-    member.email = keyboard.nextLine();
-
-    System.out.print("암호? ");
-    member.password = keyboard.nextLine();
-
-    System.out.print("사진? ");
-    member.photo = keyboard.nextLine();
-
-    System.out.print("전화? ");
-    member.tel = keyboard.nextLine();
-
-    member.registeredDate = new Date(System.currentTimeMillis());
-    
-    members[memberCount++] = member;
-    System.out.println("저장하였습니다.");
-  }
   
 
 }
