dfec236eac2b4f526faacf184197c4832e439170
eomjinyoung <jinyoung.eom@gmail.com>
2019-12-27 10:50:37 +0900

    2019-12-27

---
+++ bitcamp-java/src/main/java/com/eomcs/oop/ex02/study/Car.java
@@ -0,0 +1,34 @@
+package com.eomcs.oop.ex02.study;
+
+public class Car {
+  String name;
+  int type;
+  String color;
+  int speed;
+  int energy;
+  int power;
+  int cleanLevel;
+  
+  void speedUp() {
+    //Car this = 메서드를 호출할 때 넘겨준 인스턴스 주소;
+    if (this.speed >= 100)
+      return;
+    this.speed += 10;
+  }
+  
+  int comparePower(Car c) {
+    if (this.power > c.power)
+      return -1;
+    else if (this.power == c.power)
+      return 0;
+    else 
+      return 1;
+  }
+}
+
+
+
+
+
+
+
