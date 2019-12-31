43f2bf848afd55d2f0366e3a6f526379d0760d4e
eomjinyoung <jinyoung.eom@gmail.com>
2019-12-31 12:38:06 +0900

    2019-12-31

---
+++ bitcamp-java/src/main/java/com/eomcs/oop/ex03/test/Test02.java
@@ -0,0 +1,22 @@
+package com.eomcs.oop.ex03.test;
+
+public class Test02 {
+  
+  
+  Test02() {
+    
+    this.x = 300;
+  }
+  
+  Test02(int a) {
+    
+    this.x = 300;
+  }
+  
+  {
+    this.x = 200;
+  }
+  
+  int x;
+  
+}
