34b452c86a8c61ce582bd71ef5e3b02da4a90891
eomjinyoung <jinyoung.eom@gmail.com>
2020-01-08 11:53:38 +0900

    2020-01-08

--- bitcamp-project/src/main/java/com/eomcs/util/ArrayListTest.java
+++ bitcamp-project/src/main/java/com/eomcs/util/ArrayListTest.java
@@ -13,9 +13,23 @@ public class ArrayListTest {
     a1.add("eee");
     a1.add("fff");
     
+    System.out.println(a1.get(-1));
+    System.out.println(a1.get(6));
+    
+    String oldValue = (String) a1.set(0, "xxx");
+    System.out.printf("%s ==> %s\n", oldValue, a1.get(0));
+    
+    a1.set(-1, "yyy");
+    a1.set(6, "zzz");
+    
+    System.out.println("---------------");
+    
     print(a1);
     
-    a1.remove(0);
+    a1.remove(-1);
+    
+    oldValue = (String) a1.remove(0);
+    System.out.printf("삭제 전(%s) ==> 삭제 후(%s)\n", oldValue, a1.get(0));
     
     System.out.println("---------------");
     
@@ -23,7 +37,7 @@ public class ArrayListTest {
   }
   
   static void print(ArrayList arr) {
-    for (int i = 0; i < arr.size; i++) {
+    for (int i = 0; i < arr.size(); i++) {
       System.out.println(arr.get(i));
     }
   }
