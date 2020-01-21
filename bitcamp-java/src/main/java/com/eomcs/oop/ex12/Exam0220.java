e01181d8306880085e0dd2c66f5cde9fca8cb409
eomjinyoung <jinyoung.eom@gmail.com>
2020-01-21 10:45:48 +0900

    2020-01-21

--- bitcamp-java/src/main/java/com/eomcs/oop/ex12/Exam0220.java
+++ bitcamp-java/src/main/java/com/eomcs/oop/ex12/Exam0220.java
@@ -1,10 +1,16 @@
 // 람다(lambda) 문법
 package com.eomcs.oop.ex12;
 
-public class Exam0210 {
+public class Exam0220 {
 
   static interface Player {
     void play();
+
+    default void stop() {}
+
+    static String info() {
+      return "ok!";
+    }
   }
 
   public static void main(String[] args) {
