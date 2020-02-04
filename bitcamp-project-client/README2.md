6102dd2e8f259322a842d1ee6191e5637baf799a
eomjinyoung <jinyoung.eom@gmail.com>
2020-02-04 10:42:38 +0900

    2020-02-04

--- bitcamp-project-client/README2.md
+++ bitcamp-project-client/README2.md
@@ -1,21 +1,20 @@
-# 32_3 - LMS 명령을 입력 받는 기능을 추가하기
+# 32_3 - 게시물 관리에서 UI 기능을 분리하여 클라이언트에 적용하기 
 
 ## 학습목표
 
-- 사용자로부터 명령을 입력 받을 수 있다.
+- 클라이언트와 서버 간에 객체를 주고 받을 수 있다.
 
 ## 실습 소스 및 결과
 
+- src/main/java/com/eomcs/lms/domain/Board.java 추가
 - src/main/java/com/eomcs/lms/ClientApp.java 변경
 
 ## 실습  
 
-### 훈련 1: 31번 프로젝트의 App 클래스에서 명령을 입력 받는 부분을 가져오라.
+### 훈련 1: 31번 프로젝트에서 Board 클래스를 가져오라.
 
-- com.eomcs.util 패키지 생성한다.
-- com/eomcs/util/Prompt.java 클래스를 가져온다.
-- ClientApp.java 변경한다.
-  - 사용자가 입력한 명령을 처리하는 코드를 가져온다.
+- com.eomcs.lms.domain 패키지 생성한다.
+- Board.java 가져온다.
 
 ### 훈련 2: 31번 프로젝트에서 게시물 관리를 처리하는 Command 객체 가져오라.
 
--- bitcamp-project-client/src/main/java/com/eomcs/lms/ClientApp.java
+++ bitcamp-project-client/src/main/java/com/eomcs/lms/ClientApp.java
@@ -3,12 +3,90 @@ package com.eomcs.lms;
 
 import java.io.PrintStream;
 import java.net.Socket;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
+import com.eomcs.lms.handler.Command;
+import com.eomcs.util.Prompt;
 
 public class ClientApp {
+
+  Scanner keyboard = new Scanner(System.in);
+  Prompt prompt = new Prompt(keyboard);
+
+  public void service() {
+
+    Deque<String> commandStack = new ArrayDeque<>();
+    Queue<String> commandQueue = new LinkedList<>();
+
+    HashMap<String, Command> commandMap = new HashMap<>();
+    String command;
+
+    while (true) {
+      command = prompt.inputString("\n명령> ");
+
+      if (command.length() == 0)
+        continue;
+
+      if (command.equals("quit")) {
+        System.out.println("안녕!");
+        break;
+      } else if (command.equals("history")) {
+        printCommandHistory(commandStack.iterator());
+        continue;
+      } else if (command.equals("history2")) {
+        printCommandHistory(commandQueue.iterator());
+        continue;
+      }
+
+      commandStack.push(command);
+
+      commandQueue.offer(command);
+
+      Command commandHandler = commandMap.get(command);
+
+      if (commandHandler != null) {
+        try {
+          commandHandler.execute();
+        } catch (Exception e) {
+          e.printStackTrace();
+          System.out.printf("명령어 실행 중 오류 발생: %s\n", e.getMessage());
+        }
+      } else {
+        System.out.println("실행할 수 없는 명령입니다.");
+      }
+    }
+
+    keyboard.close();
+
+  }
+
+  private void printCommandHistory(Iterator<String> iterator) {
+    int count = 0;
+    while (iterator.hasNext()) {
+      System.out.println(iterator.next());
+      count++;
+
+      if ((count % 5) == 0) {
+        String str = prompt.inputString(":");
+        if (str.equalsIgnoreCase("q")) {
+          break;
+        }
+      }
+    }
+  }
+
   public static void main(String[] args) throws Exception {
     System.out.println("클라이언트 수업 관리 시스템입니다.");
 
+    ClientApp app = new ClientApp();
+    app.service();
+
+    /*
     String serverAddr = null;
     int port = 0;
 
@@ -65,5 +143,6 @@ public class ClientApp {
     }
 
     keyScan.close();
+    */
   }
 }
