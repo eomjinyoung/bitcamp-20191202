cfc7612bf9ae5afea89569a6300b26cdf12d3bc5
eomjinyoung <jinyoung.eom@gmail.com>
2020-02-21 13:16:00 +0900

    2020-02-21

--- bitcamp-project-client/src/main/java/com/eomcs/lms/ClientApp.java
+++ bitcamp-project-client/src/main/java/com/eomcs/lms/ClientApp.java
@@ -1,6 +1,8 @@
 // LMS 클라이언트
 package com.eomcs.lms;
 
+import java.io.PrintStream;
+import java.net.Socket;
 import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.Iterator;
@@ -57,14 +59,60 @@ public class ClientApp {
     // => 예) bitcamp://서버주소:포트번호/board/list
     //
     String host = null;
-    int port = 0;
-    String serletPath = null;
+    int port = 9999;
+    String servletPath = null;
 
+    // 명령어를 분석하여 서버주소와 포트번호, 실행시킬 작업명을 분리한다.
     try {
       if (!command.startsWith("bitcamp://")) {
         throw new Exception("명령어 형식이 옳지 않습니다!");
       }
 
+      // System.out.println(command);
+      // command 예) bitcamp://localhost:9999/board/list
+
+      String url = command.substring(10);
+      // => localhost:9999/board/list
+
+      // System.out.println(url);
+
+      int index = url.indexOf('/'); // 14
+      String[] str = //
+          url.substring(0, index) // localhost:9999
+              .split(":"); // {"localhost", "9999"}
+
+      host = str[0];
+      if (str.length == 2) {
+        port = Integer.parseInt(str[1]);
+      }
+      // System.out.printf("=> %s:%d\n", host, port); // => localhost:9999
+
+      servletPath = url.substring(index);
+      System.out.printf("=> %s\n", servletPath); // => /board/list
+
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+      return;
+    }
+
+    // 서버에 연결한다.
+    try (Socket socket = new Socket(host, port);
+        PrintStream out = new PrintStream(socket.getOutputStream());
+        Scanner in = new Scanner(socket.getInputStream())) {
+
+      // 서버에 명령을 보낸다.
+      out.println(servletPath);
+      out.flush();
+
+      // 서버의 응답을 읽어서 출력한다.
+      while (true) {
+        String response = in.nextLine();
+        if (response.equals("!end!")) {
+          break;
+        }
+
+        System.out.println(response);
+      }
 
     } catch (Exception e) {
       System.out.println(e.getMessage());
