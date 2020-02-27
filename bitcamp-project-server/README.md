72f671f68f8fac4eb5e681ebe2ca66513e59a3b9
eomjinyoung <jinyoung.eom@gmail.com>
2020-02-27 15:14:43 +0900

    2020-02-27

--- bitcamp-project-server/README.md
+++ bitcamp-project-server/README.md
@@ -1,11 +1,7 @@
-# 42_1 - SQL 삽입 공격과 자바 시큐어 코딩: 사용자 로그인 기능 추가
+# 42_2 - SQL 삽입 공격과 자바 시큐어 코딩: PreparedStatement로 전환하기
 
-DB 프로그래밍의 핵심은 JDBC API를 사용하여 SQL문을 실행하는 것이다. 
-SQL 문은 보통 사용자가 입력한 값을 가지고 작성하는데, 
-여기서 보안 문제가 발생한다. 
-SQL을 잘 아는 사용자가 입력 값에 SQL 문법을 포함시켜서 
-내부 데이터를 조회한다거나 변경할 수 있다.
-이를 방지하기 위해서는 사용자가 입력한 값을 가지고 SQL 문을 만들어서는 안된다.
+PreparedStatement를 이용하여 SQL 문과 값을 분리하여 실행하면,
+SQL 삽입 공격을 막을 수 있다.
 
 
 ## 학습목표
@@ -23,39 +19,10 @@ SQL을 잘 아는 사용자가 입력 값에 SQL 문법을 포함시켜서
 
 ## 실습  
 
-### 훈련1: 사용자 로그인 기능을 만들라.
+### 훈련1: 기존의 Statement 객체를 PreparedStatement 객체로 모두 바꿔라.
 
-- lms_member 테이블의 암호 초기화
-  - 테스트하기 위해 모든 회원의 암호를 '1111'로 초기화 한다.
-  - update lms_member set pwd=password('1111') 실행
-- com.eomcs.lms.dao.MemberDao 변경
-  - 이메일과 암호를 가지고 사용자를 조회하는 메서드를 추가한다.
-  - Member findByEmailAndPassword(String email, String password)
-- com.eomcs.lms.dao.mariadb.MemberDaoImpl 변경
-  - MemberDao에 추가한 메서드를 구현한다.
-  - insert(), update()의 SQL 문에서 암호를 입력하거나 변경할 때 
-    password() SQL 함수로 인코딩하도록 SQL 문을 변경한다.
-- com.eomcs.lms.servlet.LoginServlet 추가
-  - 사용자로부터 이메일과 암호를 입력받아 인증을 수행한다.
-- com.eomcs.lms.ServerApp 변경
-  - "/auth/login" 명령을 처리할 LoginServlet 객체를 맵에 추가한다.
-  
-'ClientApp' 실행 예:
-```
-명령> /auth/login
-이메일?
-user1@test.com
-암호?
-1111
-'홍길동'님 환영합니다.
-
-명령> /auth/login
-이메일?
-user1@test.com
-암호?
-2222
-사용자가 정보가 유효하지 않습니다.
-```
+- com.eomcs.lms.dao.mariadb.XxxDaoImpl 변경
+  - Statement를 PreparedStatement 로 변경한다.
 
 ### 훈련2: SQL 삽입 공격을 통해 유효하지 않은 사용자 정보로 로그인 해 보라.
 
--- bitcamp-project-server/src/main/java/com/eomcs/lms/dao/mariadb/BoardDaoImpl.java
+++ bitcamp-project-server/src/main/java/com/eomcs/lms/dao/mariadb/BoardDaoImpl.java
@@ -1,8 +1,8 @@
 package com.eomcs.lms.dao.mariadb;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import com.eomcs.lms.dao.BoardDao;
@@ -21,32 +21,28 @@ public class BoardDaoImpl implements BoardDao {
   public int insert(Board board) throws Exception {
 
     try (Connection con = dataSource.getConnection(); //
-        Statement stmt = con.createStatement()) {
-
-      int result = stmt.executeUpdate("insert into lms_board(conts) values('" //
-          + board.getTitle() + "')");
-
-      return result;
+        PreparedStatement stmt = con.prepareStatement(//
+            "insert into lms_board(conts) values(?)")) {
+      stmt.setString(1, board.getTitle());
+      return stmt.executeUpdate();
     }
   }
 
   @Override
   public List<Board> findAll() throws Exception {
     try (Connection con = dataSource.getConnection();
-        Statement stmt = con.createStatement();
-        ResultSet rs = stmt.executeQuery( //
-            "select board_id, conts, cdt, vw_cnt from lms_board order by board_id desc")) {
-
+        PreparedStatement stmt = con.prepareStatement(//
+            "select board_id, conts, cdt, vw_cnt" //
+                + " from lms_board" //
+                + " order by board_id desc");
+        ResultSet rs = stmt.executeQuery()) {
       ArrayList<Board> list = new ArrayList<>();
-
       while (rs.next()) {
         Board board = new Board();
-
         board.setNo(rs.getInt("board_id"));
         board.setTitle(rs.getString("conts"));
         board.setDate(rs.getDate("cdt"));
         board.setViewCount(rs.getInt("vw_cnt"));
-
         list.add(board);
       }
 
@@ -57,20 +53,22 @@ public class BoardDaoImpl implements BoardDao {
   @Override
   public Board findByNo(int no) throws Exception {
     try (Connection con = dataSource.getConnection(); //
-        Statement stmt = con.createStatement();
-        ResultSet rs = stmt.executeQuery( //
-            "select board_id, conts, cdt, vw_cnt from lms_board where board_id=" + no)) {
-
-      if (rs.next()) { // 데이터를 한 개 가져왔으면 true를 리턴한다.
-        Board board = new Board();
-        board.setNo(rs.getInt("board_id"));
-        board.setTitle(rs.getString("conts"));
-        board.setDate(rs.getDate("cdt"));
-        board.setViewCount(rs.getInt("vw_cnt"));
-        return board;
-
-      } else {
-        return null;
+        PreparedStatement stmt = con.prepareStatement(//
+            "select board_id, conts, cdt, vw_cnt" //
+                + " from lms_board" //
+                + " where board_id=?")) {
+      stmt.setInt(1, no);
+      try (ResultSet rs = stmt.executeQuery()) {
+        if (rs.next()) { // 데이터를 한 개 가져왔으면 true를 리턴한다.
+          Board board = new Board();
+          board.setNo(rs.getInt("board_id"));
+          board.setTitle(rs.getString("conts"));
+          board.setDate(rs.getDate("cdt"));
+          board.setViewCount(rs.getInt("vw_cnt"));
+          return board;
+        } else {
+          return null;
+        }
       }
     }
   }
@@ -78,24 +76,24 @@ public class BoardDaoImpl implements BoardDao {
   @Override
   public int update(Board board) throws Exception {
     try (Connection con = dataSource.getConnection(); //
-        Statement stmt = con.createStatement()) {
-
-      int result = stmt.executeUpdate("update lms_board set conts = '" + //
-          board.getTitle() + "' where board_id=" + board.getNo());
-
-      return result;
+        PreparedStatement stmt = con.prepareStatement(//
+            "update lms_board set" //
+                + " conts=?" //
+                + " where board_id=?")) {
+      stmt.setString(1, board.getTitle());
+      stmt.setInt(2, board.getNo());
+      return stmt.executeUpdate();
     }
   }
 
   @Override
   public int delete(int no) throws Exception {
     try (Connection con = dataSource.getConnection(); //
-        Statement stmt = con.createStatement()) {
-
-      int result = stmt.executeUpdate("delete from lms_board where board_id=" + no);
-
-      return result;
+        PreparedStatement stmt = con.prepareStatement(//
+            "delete from lms_board"//
+                + " where board_id=?")) {
+      stmt.setInt(1, no);
+      return stmt.executeUpdate();
     }
   }
-
 }
