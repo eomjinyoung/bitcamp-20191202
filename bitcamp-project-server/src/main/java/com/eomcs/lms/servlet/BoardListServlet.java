641e9fe86d4173f9db5276765da34cc9091e64fe
eomjinyoung <jinyoung.eom@gmail.com>
2020-03-25 13:12:58 +0900

    eomcs-java-project-54_2

--- bitcamp-project-server/src/main/java/com/eomcs/lms/servlet/BoardListServlet.java
+++ bitcamp-project-server/src/main/java/com/eomcs/lms/servlet/BoardListServlet.java
@@ -19,14 +19,34 @@ public class BoardListServlet {
 
   @RequestMapping("/board/list")
   public void service(Scanner in, PrintStream out) throws Exception {
+
+    out.println("<!DOCTYPE html>");
+    out.println("<html>");
+    out.println("<head>");
+    out.println("  <meta charset='UTF-8'>");
+    out.println("  <title>게시글 목록</title>");
+    out.println("</head>");
+    out.println("<body>");
+    out.println("  <h1>게시글</h1>");
+    out.println("  <table border='1'>");
+    out.println("  <tr>");
+    out.println("    <th>번호</th>");
+    out.println("    <th>제목</th>");
+    out.println("    <th>등록일</th>");
+    out.println("    <th>조회수</th>");
+    out.println("  </tr>");
+
     List<Board> boards = boardService.list();
     for (Board board : boards) {
-      out.printf("=> %d, %s, %s, %d\n", //
+      out.printf("  <tr><td>%d</td> <td>%s</td> <td>%s</td> <td>%d</td></tr>\n", //
           board.getNo(), //
           board.getTitle(), //
           board.getDate(), //
           board.getViewCount() //
       );
     }
+
+    out.println("</body>");
+    out.println("</html>");
   }
 }
