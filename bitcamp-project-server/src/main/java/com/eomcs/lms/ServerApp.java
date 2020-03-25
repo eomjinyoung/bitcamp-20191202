eed09c1843ea8fdd425c70590e734a42f15faf14
eomjinyoung <jinyoung.eom@gmail.com>
2020-03-25 12:44:27 +0900

    eomcs-java-project-54_1

--- bitcamp-project-server/src/main/java/com/eomcs/lms/ServerApp.java
+++ bitcamp-project-server/src/main/java/com/eomcs/lms/ServerApp.java
@@ -138,15 +138,29 @@ public class ServerApp {
         Scanner in = new Scanner(socket.getInputStream());
         PrintStream out = new PrintStream(socket.getOutputStream())) {
 
-      String request = in.nextLine();
-      logger.info(String.format("요청 명령 => %s", request));
+      String[] requestLine = in.nextLine().split(" ");
+      // 기타 나머지 요청 데이터를 버린다.
+      while (true) {
+        String line = in.nextLine();
+        if (line.length() == 0) {
+          break;
+        }
+      }
 
-      if (request.equalsIgnoreCase("/server/stop")) {
+      String method = requestLine[0];
+      String requestUri = requestLine[1];
+      logger.info(String.format("method => %s", method));
+      logger.info(String.format("request-uri => %s", requestUri));
+
+      // HTTP 응답 헤더 출력
+      printResponseHeader(out);
+
+      if (requestUri.equalsIgnoreCase("/server/stop")) {
         quit(out);
         return;
       }
 
-      RequestHandler requestHandler = handlerMapper.getHandler(request);
+      RequestHandler requestHandler = handlerMapper.getHandler(requestUri);
 
       if (requestHandler != null) {
         try {
@@ -189,6 +203,12 @@ public class ServerApp {
     out.println("OK");
     out.println("!end!");
     out.flush();
+  }
+
+  private void printResponseHeader(PrintStream out) {
+    out.println("HTTP/1.1 200 OK");
+    out.println("Server: bitcampServer");
+    out.println();
   }
 
   public static void main(String[] args) {
