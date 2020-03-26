16b81b7dfc3f7a725c056cdda1ab7a2d0a8ba45c
eomjinyoung <jinyoung.eom@gmail.com>
2020-03-26 17:23:39 +0900

    eomcs-java-project-54_2

--- bitcamp-project-server/v54_2/src/main/java/com/eomcs/lms/servlet/BoardAddFormServlet.java
+++ bitcamp-project-server/v54_2/src/main/java/com/eomcs/lms/servlet/BoardAddFormServlet.java
@@ -6,24 +6,21 @@ import org.springframework.stereotype.Component;
 import com.eomcs.util.RequestMapping;
 
 @Component
-public class MemberAddFormServlet {
+public class BoardAddFormServlet {
 
-  @RequestMapping("/member/addForm")
+  @RequestMapping("/board/addForm")
   public void service(Map<String, String> params, PrintStream out) throws Exception {
     out.println("<!DOCTYPE html>");
     out.println("<html>");
     out.println("<head>");
     out.println("<meta charset='UTF-8'>");
-    out.println("<title>회원 입력</title>");
+    out.println("<title>게시글 입력</title>");
     out.println("</head>");
     out.println("<body>");
-    out.println("<h1>회원 입력</h1>");
-    out.println("<form action='/member/add'>");
-    out.println("이름: <input name='name' type='text'><br>");
-    out.println("이메일: <input name='email' type='email'><br>");
-    out.println("암호: <input name='password' type='password'><br>");
-    out.println("사진: <input name='photo' type='text'><br>");
-    out.println("전화: <input name='tel' type='tel'><br>");
+    out.println("<h1>게시물 입력</h1>");
+    out.println("<form action='/board/add'>");
+    out.println("내용:<br>");
+    out.println("<textarea name='title' rows='5' cols='60'></textarea><br>");
     out.println("<button>제출</button>");
     out.println("</form>");
     out.println("</body>");
