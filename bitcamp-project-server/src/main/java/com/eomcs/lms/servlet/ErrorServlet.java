1e3fc7c42f1cfa4674555144264fc8ae9bd17bfc
eomjinyoung <jinyoung.eom@gmail.com>
2020-04-09 14:22:02 +0900

    56_4

--- bitcamp-project-server/src/main/java/com/eomcs/lms/servlet/ErrorServlet.java
+++ bitcamp-project-server/src/main/java/com/eomcs/lms/servlet/ErrorServlet.java
@@ -13,7 +13,7 @@ public class ErrorServlet extends HttpServlet {
   private static final long serialVersionUID = 1L;
 
   @Override
-  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+  protected void service(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
 
     response.setContentType("text/html;charset=UTF-8");
