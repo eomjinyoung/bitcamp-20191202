4dadf7c96becb414a09702093d334a947372fe2a
eomjinyoung <jinyoung.eom@gmail.com>
2020-05-07 15:41:20 +0900

    v60_1

--- bitcamp-project-server/src/main/java/com/eomcs/lms/web/AppWebConfig.java
+++ bitcamp-project-server/src/main/java/com/eomcs/lms/web/AppWebConfig.java
@@ -9,6 +9,9 @@ import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
+import org.springframework.web.servlet.view.tiles3.TilesView;
 
 // AppWebApplicationInitializer가 설정하는
 // DispatcherServlet(/app/* 요청 처리)의 IoC 컨테이너를 위한 설정이다.
@@ -29,7 +32,27 @@ public class AppWebConfig {
         "/WEB-INF/jsp/", // prefix
         ".jsp" // suffix
     );
+    vr.setOrder(2);
     return vr;
+  }
+
+  @Bean
+  public ViewResolver tilesViewResolver() {
+    UrlBasedViewResolver vr = new UrlBasedViewResolver();
+
+    // Tiles 설정에 때라 템플릿을 실행할 뷰 처리기를 등록한다.
+    vr.setViewClass(TilesView.class);
+
+    // 뷰리졸버의 우선 순위를 InternalResourceViewResolver 보다 우선하게 한다.
+    vr.setOrder(1);
+    return vr;
+  }
+
+  @Bean
+  public TilesConfigurer tilesConfigurer() {
+    TilesConfigurer configurer = new TilesConfigurer();
+    configurer.setDefinitions("/WEB-INF/defs/tiles.xml");
+    return configurer;
   }
 
   @Bean
---
+++ bitcamp-project-server/src/main/webapp/WEB-INF/tiles/template.jsp
@@ -0,0 +1,44 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"
+    trimDirectiveWhitespaces="true"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib uri="http://tiles.apache.org/tags-tiles" prefix="tiles" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset='UTF-8'>
+<c:if test="${not empty refreshUrl}">
+<meta http-equiv="Refresh" content="${refreshUrl}">
+</c:if>
+<title>Bitcamp-LMS</title>
+<link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css' integrity='sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh' crossorigin='anonymous'>
+<style>
+body {
+  background-color: LightGray;
+}
+div.container {
+  background: white;
+  border: 1px solid gray;
+  width: 600px;
+}
+footer {
+  margin-top: 20px;
+  padding: 10px;
+  background-color: navy;
+  color: white;
+  text-align: center;
+}
+</style>
+</head>
+<body>
+
+<tiles:insertAttribute name="header"/>
+
+<div class='container'>
+<tiles:insertAttribute name="body"/>
+</div>
+
+<tiles:insertAttribute name="footer"/>
+
+</body>
+</html>
